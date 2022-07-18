527aec7265be415dd7ae2dfab6a5deb1b911983d
beruangs <creamyberuang@gmail.com>
2022-07-19 06:21:33 +0700

    Initial commit

---
+++ function.js
@@ -0,0 +1,22 @@
+alert("Pencet Kue nya 🎂");
+
+// audio
+var audio = document.getElementById("HBD").autoplay;
+
+// buton
+const cake = document.getElementById(`cake`);
+const openbtn = document.getElementById(`openbtn`);
+const modal_container = document.getElementById(`modal_container`);
+const closebtn = document.getElementById(`closebtn`);
+
+cake.addEventListener(`click`, () => {
+    openbtn.classList.add(`show`);
+});
+
+openbtn.addEventListener(`click`, () => {
+    modal_container.classList.add(`show`);
+});
+
+closebtn.addEventListener(`click`, () => {
+    modal_container.classList.remove(`show`);
+});
